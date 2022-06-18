95880a571ad1dfeabbe965ebf669ce9e8be5442a
kosciolek <45483493+kosciolek@users.noreply.github.com>
2022-06-18 18:25:57 +0200

    2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ key: "value" });
+  res.json({ key: "value 2" });
 });
 
 const port = process.env.PORT ?? 3000;
